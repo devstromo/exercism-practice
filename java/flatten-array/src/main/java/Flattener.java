57ca4f2489f814f8d6248642e5d0d15bdc0d8554
alemoles <alemolerio@gmail.com>
2023-04-05 20:24:06 -0300

    Flatten Array | Solution

--- java/flatten-array/src/main/java/Flattener.java
+++ java/flatten-array/src/main/java/Flattener.java
@@ -1,10 +1,28 @@
-/*
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class Flattener {
 
-Please remove this comment when submitting your solution.
+    public List<Object> flatten(List<?> two) {
+        return flattenList(two).filter(Objects::nonNull).collect(Collectors.toList());
+    }
 
-*/
+    public static <T> Stream<T> flattenList(List<?> nestedList) {
+        return nestedList.stream().flatMap(new FlatMapper<>());
+    }
+
+    private static class FlatMapper<T> implements Function<Object, Stream<T>> {
+        @SuppressWarnings("unchecked")
+        @Override
+        public Stream<T> apply(Object input) {
+            if (input instanceof List) {
+                return flattenList((List<Object>) input);
+            } else {
+                return Stream.of((T) input);
+            }
+        }
+    }
+}
