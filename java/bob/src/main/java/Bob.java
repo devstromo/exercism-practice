3e4d5f6b8124c84aa4c4af24dd1fdc17a57bfae0
alemoles <alemolerio@gmail.com>
2023-04-12 20:03:26 -0300

    Bob | Solution

--- java/bob/src/main/java/Bob.java
+++ java/bob/src/main/java/Bob.java
@@ -1,10 +1,38 @@
-/*
+class Bob {
+    public String hey(String input) {
+        if (input == null || input.trim()
+          .isEmpty()) {
+            return "Fine. Be that way!";
+        }
+        var hasLetters = hasLetters(input.trim());
+        var isUpperString = hasLetters && isUppercase(input.trim());
+        var isQuestion = isQuestion(input.trim());
+        if (isQuestion && isUpperString) {
+            return "Calm down, I know what I'm doing!";
+        } else if (isUpperString) {
+            return "Whoa, chill out!";
+        } else if (isQuestion) {
+            return "Sure.";
+        }
+        return "Whatever.";
+    }
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    private boolean isUppercase(String input) {
+        return input.chars()
+          .filter(Character::isAlphabetic)
+          .mapToObj(value -> (char) value)
+          .allMatch(Character::isUpperCase);
+    }
 
-Please remove this comment when submitting your solution.
+    private boolean hasLetters(String input) {
+        return input.chars()
+          .filter(Character::isAlphabetic)
+          .mapToObj(value -> (char) value)
+          .findAny()
+          .isPresent();
+    }
 
-*/
+    private boolean isQuestion(String input) {
+        return input.charAt(input.length() - 1) == '?';
+    }
+}
