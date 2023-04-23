e8b9dbf29adec517ca96602011aa2810f11feff5
alemoles <alemolerio@gmail.com>
2023-04-23 12:50:22 -0300

    Squeaky Clean

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,34 +1,25 @@
-import java.util.regex.Pattern;
-
 class SqueakyClean {
     static String clean(String identifier) {
-        identifier = replaceControl(identifier);
-        identifier = replaceNoLetters(identifier);
-        identifier = replaceSpace(identifier);
-        identifier = replaceGreeksUpper(identifier);
-        identifier = kebabToCamel(identifier);
-        return identifier;
-    }
+        var result = new StringBuilder();
 
-    private static String replaceSpace(String identifier) {
-        return identifier.replaceAll(" ", "_");
-    }
+        var kebabCase = false;
 
-    private static String replaceControl(String identifier) {
-        return identifier.replaceAll("\\p{C}", "CTRL").replaceAll("[\\r\\n\\t]", "CTRL");
-    }
-
-    private static String kebabToCamel(String identifier) {
-        return Pattern.compile("-(.)")
-            .matcher(identifier)
-            .replaceAll(mr -> mr.group(1)
-                .toUpperCase());
-    }
-
-    private static String replaceNoLetters(String identifier) {
-        return identifier.replaceAll("[^\\p{L} ]", "");
-    }
-    private static String replaceGreeksUpper(String identifier) {
-        return identifier.replaceAll("[\u03B1-\u03C9]", "");
+        for (var c : identifier.toCharArray()) {
+            if (c == ' ') {
+                result.append('_');
+            } else if (Character.isISOControl(c)) {
+                result.append("CTRL");
+            } else if (c == '-') {
+                kebabCase = true;
+            } else if (Character.isLetter(c)) {
+                if (kebabCase) {
+                    result.append(Character.toTitleCase(c));
+                    kebabCase = false;
+                } else if (c < 'α' || c > 'ω') {
+                    result.append(c);
+                }
+            }
+        }
+        return result.toString();
     }
 }
