4f570f958dac59e8b6d9a05fcf549514b8a31e87
alemoles <alemolerio@gmail.com>
2023-06-22 21:48:22 -0300

    Exercism | ISBN Verifier

--- java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,7 +1,28 @@
 class IsbnVerifier {
 
     boolean isValid(String stringToVerify) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        var isbn = stringToVerify.replace("-", "");
+        if (isbn.length() != 10) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            int digit = isbn.charAt(i) - '0';
+            if (digit < 0 || digit > 9) {
+                return false;
+            }
+            sum += (digit * (10 - i));
+        }
+
+        char lastChar = isbn.charAt(9);
+        if (lastChar != 'X' && (lastChar < '0' || lastChar > '9')) {
+            return false;
+        }
+
+        sum += (lastChar == 'X' ? 10 : (lastChar - '0'));
+
+        return (sum % 11 == 0);
     }
 
 }
